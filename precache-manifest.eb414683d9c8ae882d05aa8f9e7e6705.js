4bbf28300f769d72b32a2a2188d055f958c847dc
eden varsulker <edenv30@gmail.com>
2020-06-08 01:09:05 +0300

    Updates

---
+++ precache-manifest.eb414683d9c8ae882d05aa8f9e7e6705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05871a16cd962ecbe4dd8d52ba42a66",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "4b812ce7428bc54f6b66",
+    "url": "/TicTacToe/static/css/main.75f50142.chunk.css"
+  },
+  {
+    "revision": "e77116ea80b59ee67d78",
+    "url": "/TicTacToe/static/js/2.dd72d2c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.dd72d2c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b812ce7428bc54f6b66",
+    "url": "/TicTacToe/static/js/main.782fa356.chunk.js"
+  },
+  {
+    "revision": "54af3b5cfa1a5ca204c1",
+    "url": "/TicTacToe/static/js/runtime-main.794d048a.js"
+  }
+]);
